5216968662d6e79dfac04f841f323e4267f61a07
aw-glaronia <49639125+aw-glaronia@users.noreply.github.com>
2019-04-27 02:06:21 +0200

    Create check_nmap.sh

---
+++ check_nmap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+####
+#	Author:		Andreas Walker <a.walker@glaronia.ch>
+#	Date:		27-Apr-2019
+#	Version:	0.0.1
+#	License:	GPL
+####
+
+#Initiating Variable
+HOST=$1
+
+#Die if host variable is empty
+if  [ -z "$HOST" ] 
+	then
+	echo "UNKNOWN - host not defined"
+	exit 3
+fi
+
+#Store NMAP-Output as Variable
+NMAP=$(nmap -sP $HOST)
+
+#Get Status
+STATUS=$(echo "$NMAP" | grep "Host is")
+
+#Return OK if Host is Up. else Critical.
+if [[ "$STATUS" == *"up"* ]];
+	then
+	echo "OK - $HOST: $STATUS"
+	exit 0
+fi
+DOWN=$(echo "$NMAP" | grep "0 hosts up")
+if [[ "$DOWN" == *"0 hosts up"* ]]
+	then
+	echo "CRITICAL - $HOST: $DOWN"
+	exit 2
+fi
